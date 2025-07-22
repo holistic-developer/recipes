342e5d72c4d1309c90daec43b24ffa7bf4b77613
Andreas Taranetz <57600104+holistic-developer@users.noreply.github.com>
2025-07-22 07:23:51 +0000

    Add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://holistic-developer.github.io',
+    base: 'recipes',
+});
