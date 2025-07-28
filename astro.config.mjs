62396ce3db7caacaf341f41146b98ccd9b9e62c1
Andreas Taranetz <andreas.taranetz@dynatrace.com>
2025-07-28 10:57:17 +0200

    Move CNAME file to public folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rezepte.taranetz.com',
+	site: 'https://rezepte.taranetz.com',
 	prefetch: true,
 });
