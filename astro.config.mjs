a679e6dc20bd9f6d677e552118c90766ab6b0420
Andreas Taranetz <andreas.taranetz@dynatrace.com>
2025-11-25 18:51:46 +0100

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://rezepte.taranetz.com',
-	prefetch: true,
+  site: 'https://rezepte.taranetz.com',
+  prefetch: true,
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
